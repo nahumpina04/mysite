faa78d767c9f98babbe831ccf5f2b5c14b1560d6
Nahum Pina <npina@talleymetal.com>
2025-07-28 14:32:47 -0700

    3D and AOS

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/QOg0XYUGtjXbquD8/scene.splinecode" 
+        width={400}
+        height={512}
+      />
+    </main>
+  );
+}
